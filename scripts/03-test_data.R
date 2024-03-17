1d81930c37b16a97b52cc8619f8f4b1a32dd6c00
Niyer02 <niyer@hotmail.ca>
2024-03-16 22:08:30 -0400

    Added tests

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,32 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the data to ensure the types are correct
+# Author: Nikhil Iyer
+# Date: 16 March 2024
+# Contact: nik.iyer@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Dependancies Installed
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(assertthat)
 
 #### Test data ####
+analysis_data <- read_csv("data/analysis_data/cleaned_ontario_data.csv")
+
+# Test columns are numeric
+test_numeric <- function(data) {
+  ref_date <- all(sapply(data$REF_DATE, is.numeric))
+  value <- all(sapply(data$VALUE, is.numeric))
+  
+  if (!ref_date) {
+    stop("Column 'REF_DATE' is not numeric.")
+  }
+  if (!value) {
+    stop("Column 'VALUE' is not numeric.")
+  }
+}
+
+# Run the test
+test_numeric(analysis_data)
+
